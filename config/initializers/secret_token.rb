8fc6e4aff9081b36779ceb22b17b7bde9c63ce49
Mike Moore <mike@blowmage.com>
2013-08-14 12:15:02 -0600

    New rails app
    $ rails new comics

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Comics::Application.config.secret_key_base = 'd0dbd4b95880e475ba515867839ce8fb3558be2ca1ffc78c3ffff9b988d2daed120f47c7857527b0f6d1aa598d16d61614f2df4064ff157a386be5ec79ec6a42'
